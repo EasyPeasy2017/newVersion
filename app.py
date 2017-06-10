946bc829d8eadc149387bde1fe38a1239faa6d5c
nickdg <delgrosso@bio.lmu.de>
2017-06-10 21:05:46 +0200

    added CORS support

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask
 from flask_restful import Resource, Api
+from flask_cors import CORS, cross_origin
 from easypeasy import query_pons_dictionary, extract_definitions, secret
 import re
 from os import path
 
 app = Flask(__name__)
+CORS(app)
 api = Api(app)
 
 
